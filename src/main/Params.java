374da5d125f5f11c3d327d11fdd169632afd6958
iio <iio@sce.iit.tsukuba.ac.jp>
2020-12-21 23:28:02 +0900

    First commit.

---
+++ src/main/Params.java
@@ -0,0 +1,54 @@
+package main;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Params {
+
+	public static final String robotType;
+	public static final int readPort;
+
+	static {
+		Properties properties = loadProperties("System.properties");
+		// Robot type
+		robotType = properties.getProperty("ROBOT_TYPE");
+		checkNullProperties(robotType);
+		// Server port
+		final String strReadPort = properties.getProperty("READ_PORT");
+		checkNullProperties(strReadPort);
+		checkIntProperties(strReadPort);
+		readPort = Integer.valueOf(strReadPort);
+	}
+
+	private static Properties loadProperties(String path) {
+		Properties properties = new Properties();
+		File file = new File(path);
+		try (InputStream input = new FileInputStream(file)) {
+			properties.load(input);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		return properties;
+	}
+
+	private static void checkNullProperties(String s) {
+		if (s == null) {
+			System.err.println("System.properties has no ["+ s +"].");
+			System.exit(0);
+		}
+	}
+
+	private static void checkIntProperties(String s) {
+		try {
+			Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+}
